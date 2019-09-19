b042c2be90c1413a7b7510184bfec22979b2d620
Nxymu <jchung@roc-dev.com>
2019-09-19 14:35:07 +0200

    Test

--- src/h08/h08_1.java
+++ src/h08/h08_1.java
@@ -1,5 +1,47 @@
 package h08;
 
-public class h08_1 {
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.*;
+
+public class h08_1 extends Applet {
+
+    Button Okknop;
+    Button Resetknop;
+    TextField tekstvak;
+    Label label;
+
+    public void init(){
+        setSize(500, 200);
+        label = new Label("Search");
+        add(label);
+        Okknop = new Button();
+        Okknop.setLabel( "Ok" );
+        Okknop.addActionListener(new OkKnopListener());
+        add(Okknop);
+        tekstvak = new TextField("Type here...",20);
+        add(tekstvak);
+
+        Resetknop = new Button();
+        Resetknop.setLabel( "Reset" );
+        Resetknop.addActionListener(new ResetKnopListener());
+        add(Resetknop);
+
+
+    }
+
+    class OkKnopListener implements ActionListener{
+        public void actionPerformed( ActionEvent e ) {
+            tekstvak.setText("Sorry, try again");
+            repaint();
+        }
+    }
+
+    class ResetKnopListener implements ActionListener{
+        public void actionPerformed( ActionEvent e ){
+            tekstvak.setText(" ");
+        }
+    }
 
 }
