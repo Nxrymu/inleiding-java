9028146aad4fdba0364984004b96edf10d06343c
Nxymu <jchung@roc-dev.com>
2019-10-03 13:36:40 +0200

    Test

---
+++ src/h11/h11_1.java
@@ -0,0 +1,28 @@
+package h11;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class h11_1 extends Applet {
+
+public void init(){
+
+    setSize(200,300);
+
+}
+
+public void paint(Graphics g){
+
+    g.drawLine(119, 170, 119, 90);
+    g.drawLine(123,170,123,90);
+    g.drawLine(127,170,127,90);
+    g.drawLine(131,170,131,90);
+    g.drawLine(135,170,135,90);
+    g.drawLine(139,170,139,90);
+    g.drawLine(143,170,143,90);
+    g.drawLine(147,170,147,90);
+    g.drawLine(151,170,151,90);
+    g.drawLine(155,170,155,90);
+
+}
+}
